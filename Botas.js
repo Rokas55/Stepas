15af751c68ccf941348bd372ebe809317b33d2f7
Rokas55 <lordas282@gmail.com>
2017-09-22 10:45:59 -0700

    Update and rename Bot.js to Botas.js

--- Botas.js
+++ Botas.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/javascripto/basicBot-v2/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Rokas55/Stepas/master/Kalbos.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
